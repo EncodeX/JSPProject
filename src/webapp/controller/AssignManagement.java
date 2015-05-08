26d3e69fde994aa5a57f3cdc5b8de70298fbe576
JacobKong <947124563@qq.com>
2015-05-08 20:16:21 +0800

    views

--- src/webapp/controller/AssignManagement.java
+++ src/webapp/controller/AssignManagement.java
@@ -9,11 +9,30 @@ import org.springframework.web.servlet.ModelAndView;
  * Created by zz on 2015/4/28.
  */
 @Controller
-@RequestMapping("/assman")
+@RequestMapping("/quotaman")
 public class AssignManagement {
 
     @RequestMapping(method = {RequestMethod.GET,RequestMethod.POST})
     public ModelAndView getPage(ModelAndView modelAndView){
         return modelAndView;
     }
+
+    @RequestMapping(value = "/recomquota")
+    public ModelAndView recomquota (ModelAndView modelAndView){
+        modelAndView.setViewName("/quotaman/recomquota");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/firstquota")
+    public ModelAndView firstquota (ModelAndView modelAndView){
+        modelAndView.setViewName("/quotaman/firstquota");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/finalquota")
+    public ModelAndView finalquota (ModelAndView modelAndView){
+        modelAndView.setViewName("/quotaman/finalquota");
+        return modelAndView;
+    }
+
 }
--- src/webapp/controller/RecommendManagement.java
+++ src/webapp/controller/RecommendManagement.java
@@ -9,11 +9,54 @@ import org.springframework.web.servlet.ModelAndView;
  * Created by zz on 2015/4/28.
  */
 @Controller
-@RequestMapping("/recman")
+@RequestMapping("/departman")
 public class RecommendManagement {
 
     @RequestMapping(method = {RequestMethod.GET,RequestMethod.POST})
     public ModelAndView getPage(ModelAndView modelAndView){
         return modelAndView;
     }
+
+    @RequestMapping(value = "/citysciassoc")
+    public ModelAndView citysciassoc (ModelAndView modelAndView){
+        modelAndView.setViewName("/departman/citysciassoc");
+        return modelAndView;
+    }
+
+
+    @RequestMapping(value = "/sciassoc")
+    public ModelAndView sciassoc (ModelAndView modelAndView){
+        modelAndView.setViewName("/departman/sciassoc");
+        return modelAndView;
+    }
+
+
+    @RequestMapping(value = "/engassoc")
+    public ModelAndView engassoc (ModelAndView modelAndView){
+        modelAndView.setViewName("/departman/engassoc");
+        return modelAndView;
+    }
+
+
+    @RequestMapping(value = "/agriassoc")
+    public ModelAndView agriassoc (ModelAndView modelAndView){
+        modelAndView.setViewName("/departman/agriassoc");
+        return modelAndView;
+    }
+
+
+    @RequestMapping(value = "/mediassoc")
+    public ModelAndView mediassoc (ModelAndView modelAndView){
+        modelAndView.setViewName("/departman/mediassoc");
+        return modelAndView;
+    }
+
+
+    @RequestMapping(value = "/crossassoc")
+    public ModelAndView crossassoc (ModelAndView modelAndView){
+        modelAndView.setViewName("/departman/crossassoc");
+        return modelAndView;
+    }
+
+
 }
