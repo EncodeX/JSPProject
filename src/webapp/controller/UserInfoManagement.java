362c4d71ec7b84a73a4e6e5e10d6409450b4dcbf
hehe <980182371@qq.com>
2015-05-12 00:22:37 +0800

    睡觉

--- src/webapp/controller/UserInfoManagement.java
+++ src/webapp/controller/UserInfoManagement.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import webapp.dao.ExpertDao;
+import webapp.dao.impl.ExpertDaoImpl;
+
+import java.sql.Timestamp;
 
 /**
  * Created by zz on 2015/4/28.
@@ -11,10 +15,17 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/userinfo")
 public class UserInfoManagement {
+    ExpertDao ExpertDao=new ExpertDaoImpl();
 
     @RequestMapping(value = "main",method = {RequestMethod.GET,RequestMethod.POST})
     public ModelAndView getMainPage(ModelAndView modelAndView){
         modelAndView.setViewName("userinfo/main");
+        iniExpertDateToModelAndView(modelAndView);
+        modelAndView.addObject("ischange",false);
         return modelAndView;
     }
+    private void iniExpertDateToModelAndView(ModelAndView modelAndView){
+
+    }
+
 }
