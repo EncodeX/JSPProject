006014beb09b601fbd29e47c24511527c114cef7
hehe <980182371@qq.com>
2015-05-12 00:01:13 +0800

    不好意思，刚才传的有问题

---
+++ src/webapp/model/Units.java
@@ -0,0 +1,45 @@
+package webapp.model;
+
+/**
+ * Created by Administrator on 2015/5/7.
+ */
+public class Units {
+    private int unitsID;
+    private String unitsName;
+    private String unitsPwd;
+    private int recTotal;
+
+    public int getUnitsID() {
+        return unitsID;
+    }
+
+    public void setUnitsID(int unitsID) {
+        this.unitsID = unitsID;
+    }
+
+    public String getUnitsName() {
+        return unitsName;
+    }
+
+    public void setUnitsName(String unitsName) {
+        this.unitsName = unitsName;
+    }
+
+    public String getUnitsPwd() {
+        return unitsPwd;
+    }
+
+    public void setUnitsPwd(String unitsPwd) {
+        this.unitsPwd = unitsPwd;
+    }
+
+    public int getRecTotal() {
+        return recTotal;
+    }
+
+    public void setRecTotal(int recTotal) {
+        this.recTotal = recTotal;
+    }
+
+
+}
