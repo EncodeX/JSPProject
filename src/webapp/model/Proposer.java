4d5a932f5168e6ff45efa8ff98880d3470f2c543
zz <zhaozhuo1112@qq.com>
2015-05-15 19:32:15 +0800

    学科管理后台基本完成

---
+++ src/webapp/model/Proposer.java
@@ -0,0 +1,17 @@
+package webapp.model;
+
+/**
+ * Created by zz on 2015/5/15.
+ */
+public class Proposer {
+    int userID;
+    String userName;
+    String userPwd;
+    String name;
+    String subClass;
+    int subID;
+    int recID;
+    int recResult;
+    int firCount;
+    int lasResult;
+}
